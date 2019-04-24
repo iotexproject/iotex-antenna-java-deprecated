2d3a1d75bdffc83c9a6541cf8b2b68f1f038e389
Fabryprog <fabryprog@gmail.com>
2019-04-24 16:30:57 +0200

    code style (test files)

--- src/test/java/org/iotexproject/antenna/grpc/ClientImplTest.java
+++ src/test/java/org/iotexproject/antenna/grpc/ClientImplTest.java
@@ -18,11 +18,12 @@ import org.junit.Test;
 import org.pmw.tinylog.Logger;
 
 /**
- *  Copyright (c) 2019 IoTeX
- *  This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
- *  warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
- *  permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
- *  License 2.0 that can be found in the LICENSE file.
+ * Copyright (c) 2019 IoTeX This is an alpha (internal) release and is not
+ * suitable for production. This source code is provided 'as is' and no
+ * warranties are given as to title or non-infringement, merchantability or
+ * fitness for purpose and, to the extent permitted by law, all liability for
+ * your use of the code is disclaimed. This source code is governed by Apache
+ * License 2.0 that can be found in the LICENSE file.
  * 
  * @author Fabrizio Spataro <fabryprog@gmail.com>
  */
@@ -30,19 +31,19 @@ public class ClientImplTest {
 
 	private static final String HOST = "api.iotex.one";
 	private static final Integer PORT = 80;
-	
+
 	@Test
 	public void getAccount() {
 		final String address = "io126xcrjhtp27end76ac9nmx6px2072c3vgz6suw";
 		ClientImpl browser = new ClientImpl(HOST, PORT);
 		try {
 			GetAccountResponse response = browser.getAccount(address);
-		    Logger.info(response);
-	
+			Logger.info(response);
+
 			Assert.assertNotNull(response);
 			Assert.assertNotNull(response.getAccountMeta());
 			Assert.assertEquals(address, response.getAccountMeta().getAddress());
-			
+
 			Assert.assertNotNull(response.getAccountMeta().getBalance());
 			Assert.assertNotNull(response.getAccountMeta().getNonce());
 			Assert.assertNotNull(response.getAccountMeta().getPendingNonce());
@@ -50,28 +51,28 @@ public class ClientImplTest {
 			browser.close();
 		}
 	}
-	
+
 	@Test
 	public void getChainMeta() {
 		ClientImpl browser = new ClientImpl(HOST, PORT);
 		try {
 			GetChainMetaResponse response = browser.getChainMeta();
-		    Logger.info(response);
-			
+			Logger.info(response);
+
 			Assert.assertNotNull(response);
 		} finally {
 			browser.close();
 		}
 	}
-	
+
 	@Test
 	public void GetEpochMeta() {
 		final long epoch = 1;
 		ClientImpl browser = new ClientImpl(HOST, PORT);
 		try {
 			GetEpochMetaResponse response = browser.getEpochMeta(epoch);
-		    Logger.info(response);
-			
+			Logger.info(response);
+
 			Assert.assertNotNull(response);
 			Assert.assertNotNull(response.getEpochData());
 			Assert.assertEquals(epoch, response.getEpochData().getNum());
@@ -79,13 +80,13 @@ public class ClientImplTest {
 			browser.close();
 		}
 	}
-	
+
 	@Test
 	public void getServerMeta() {
 		ClientImpl browser = new ClientImpl(HOST, PORT);
 		try {
 			GetServerMetaResponse response = browser.getServerMeta();
-		    Logger.info(response);
+			Logger.info(response);
 
 			Assert.assertNotNull(response);
 			Assert.assertNotNull(response.getServerMeta());
@@ -94,19 +95,19 @@ public class ClientImplTest {
 			Assert.assertNotNull(response.getServerMeta().getGitStatus());
 			Assert.assertNotNull(response.getServerMeta().getGoVersion());
 			Assert.assertNotNull(response.getServerMeta().getBuildTime());
-			
+
 		} finally {
 			browser.close();
 		}
 	}
-	
+
 	@Test
 	public void getBlockMetasByIndexLenghtOne() {
-		//INDEX
+		// INDEX
 		ClientImpl browser = new ClientImpl(HOST, PORT);
 		try {
 			GetBlockMetasResponse response = browser.getBlockMetasByIndex(10L, 1L);
-		    Logger.info(response);
+			Logger.info(response);
 
 			Assert.assertNotNull(response);
 			Assert.assertEquals(response.getBlkMetasList().size(), 1);
@@ -117,11 +118,11 @@ public class ClientImplTest {
 
 	@Test
 	public void getBlockMetasByIndexLenghtTen() {
-		//INDEX
+		// INDEX
 		ClientImpl browser = new ClientImpl(HOST, PORT);
 		try {
 			GetBlockMetasResponse response = browser.getBlockMetasByIndex(10L, 10L);
-		    Logger.info(response);
+			Logger.info(response);
 			Assert.assertNotNull(response);
 			Assert.assertEquals(response.getBlkMetasList().size(), 10);
 		} finally {
@@ -131,12 +132,12 @@ public class ClientImplTest {
 
 	@Test
 	public void getBlockMetasByIndexLenghtZero() {
-		//INDEX
+		// INDEX
 		ClientImpl browser = new ClientImpl(HOST, PORT);
 		try {
 			GetBlockMetasResponse response = browser.getBlockMetasByIndex(10L, 0L);
-		    Logger.info(response);
-		    
+			Logger.info(response);
+
 			Assert.assertNotNull(response);
 			Assert.assertEquals(response.getBlkMetasList().size(), 0);
 		} finally {
@@ -147,19 +148,19 @@ public class ClientImplTest {
 	@Test
 	public void getBlockMetasByHash() {
 		String hash = null;
-		//Retrieve hash from blockchain
+		// Retrieve hash from blockchain
 		ClientImpl browser = new ClientImpl(HOST, PORT);
 		try {
 			GetBlockMetasResponse response = browser.getBlockMetasByIndex(10L, 1L);
 			hash = response.getBlkMetas(0).getHash();
 
-			//force close stream
+			// force close stream
 			browser.close();
-			//reopen same stream
+			// reopen same stream
 			browser = new ClientImpl(HOST, PORT);
 			response = browser.getBlockMetasByHash(hash);
-		    Logger.info(response);
-			
+			Logger.info(response);
+
 			Assert.assertNotNull(response);
 			Assert.assertEquals(response.getBlkMetasList().size(), 1);
 			Assert.assertEquals(response.getBlkMetas(0).getHash(), hash);
@@ -167,21 +168,21 @@ public class ClientImplTest {
 			browser.close();
 		}
 	}
-	
+
 	@Test
 	public void getSuggestGasPrice() {
 		ClientImpl browser = new ClientImpl(HOST, PORT);
 		try {
 			SuggestGasPriceResponse response = browser.getSuggestGasPrice();
-		    Logger.info(response);
-		    
+			Logger.info(response);
+
 			Assert.assertNotNull(response);
 			Assert.assertEquals(response.getGasPrice(), 1000000000000L);
 		} finally {
 			browser.close();
 		}
 	}
-	
+
 	@Test
 	public void readContract() {
 		ClientImpl browser = new ClientImpl(HOST, PORT);
@@ -189,12 +190,12 @@ public class ClientImplTest {
 			Long start = 0L;
 			Long count = 30L;
 			GetActionsResponse response = browser.getActionsByIndex(start, count);
-		    Logger.info(response);
-		    
+			Logger.info(response);
+
 			Assert.assertNotNull(response);
 			Assert.assertNotNull(response.getActionInfoList());
 			Assert.assertEquals(response.getActionInfoList().size(), count.intValue());
-			for(ActionInfo item : response.getActionInfoList()) {
+			for (ActionInfo item : response.getActionInfoList()) {
 				Assert.assertNotNull(item.getActHash());
 				Assert.assertNotNull(item.getBlkHash());
 				Assert.assertNotNull(item.getAction());
@@ -212,8 +213,8 @@ public class ClientImplTest {
 		ClientImpl browser = new ClientImpl(HOST, PORT);
 		try {
 			GetActionsResponse response = browser.getActionsByIndex(10L, 1L);
-		    Logger.info(response);
-		    
+			Logger.info(response);
+
 			Assert.assertNotNull(response);
 			Assert.assertNotNull(response.getActionInfoList());
 			Assert.assertEquals(response.getActionInfoList().size(), 1);
@@ -221,13 +222,14 @@ public class ClientImplTest {
 			browser.close();
 		}
 	}
+
 	@Test
 	public void getActionsByIndexTen() {
 		ClientImpl browser = new ClientImpl(HOST, PORT);
 		try {
 			GetActionsResponse response = browser.getActionsByIndex(10L, 10L);
-		    Logger.info(response);
-		    
+			Logger.info(response);
+
 			Assert.assertNotNull(response);
 			Assert.assertNotNull(response.getActionInfoList());
 			Assert.assertEquals(response.getActionInfoList().size(), 10);
@@ -235,13 +237,14 @@ public class ClientImplTest {
 			browser.close();
 		}
 	}
+
 	@Test
 	public void getActionsByIndexZero() {
 		ClientImpl browser = new ClientImpl(HOST, PORT);
 		try {
 			GetActionsResponse response = browser.getActionsByIndex(10L, 0L);
-		    Logger.info(response);
-		    
+			Logger.info(response);
+
 			Assert.assertNotNull(response);
 			Assert.assertNotNull(response.getActionInfoList());
 			Assert.assertEquals(response.getActionInfoList().size(), 0);
@@ -249,13 +252,15 @@ public class ClientImplTest {
 			browser.close();
 		}
 	}
+
 	@Test
 	public void getActionsByHash() {
 		ClientImpl browser = new ClientImpl(HOST, PORT);
 		try {
-			GetActionsResponse response = browser.getActionsByHash("eb0bab335c3ab0d43020cbe3aa494933ee473394d75a52fd5112cff6af2a5d92", false);
-		    Logger.info(response);
-		    
+			GetActionsResponse response = browser
+					.getActionsByHash("eb0bab335c3ab0d43020cbe3aa494933ee473394d75a52fd5112cff6af2a5d92", false);
+			Logger.info(response);
+
 			Assert.assertNotNull(response);
 			Assert.assertNotNull(response.getActionInfoList());
 			Assert.assertEquals(response.getActionInfoList().size(), 1);
@@ -263,21 +268,22 @@ public class ClientImplTest {
 			browser.close();
 		}
 	}
+
 	@Test
 	public void getActionsByBlockHash() {
 		ClientImpl browser = new ClientImpl(HOST, PORT);
 		try {
 			GetBlockMetasResponse response = browser.getBlockMetasByIndex(10L, 1L);
-		    Logger.info(response);
+			Logger.info(response);
 			Assert.assertNotNull(response);
 			Assert.assertNotNull(response.getBlkMetasList());
 			Assert.assertEquals(response.getBlkMetasList().size(), 1);
 			String hash = response.getBlkMetasList().get(0).getHash();
 			browser.close();
-			
+
 			browser = new ClientImpl(HOST, PORT);
 			GetActionsResponse respAction = browser.getActionsByBlock(hash, 0L, 1L);
-		    
+
 			Assert.assertNotNull(respAction);
 			Assert.assertNotNull(respAction.getActionInfoList());
 			Assert.assertEquals(respAction.getActionInfoList().size(), 1);
@@ -285,7 +291,8 @@ public class ClientImplTest {
 			browser.close();
 		}
 	}
-	//TODO TEST
+
+	// TODO TEST
 	public void estimateGasForAction() {
 		ClientImpl browser = new ClientImpl(HOST, PORT);
 		try {
@@ -293,22 +300,24 @@ public class ClientImplTest {
 			Assert.assertNotNull(respBlock);
 			Assert.assertNotNull(respBlock.getBlkMetasList());
 			Assert.assertEquals(1, respBlock.getBlkMetasList().size());
-			
+
 			browser.close();
 			browser = new ClientImpl(HOST, PORT);
-			
-			GetActionsResponse respAction = browser.getActionsByBlock(respBlock.getBlkMetasList().get(0).getHash(), 0L, 1L);
+
+			GetActionsResponse respAction = browser.getActionsByBlock(respBlock.getBlkMetasList().get(0).getHash(), 0L,
+					1L);
 			Assert.assertNotNull(respAction);
 			Assert.assertNotNull(respAction.getActionInfoList());
 			Assert.assertEquals(1, respAction.getActionInfoList().size());
-			
+
 			browser.close();
 			browser = new ClientImpl(HOST, PORT);
-			
-			EstimateGasForActionResponse response = browser.estimateGasForAction(respAction.getActionInfoList().get(0).getAction());
-			
-		    Logger.info(response);
-		    
+
+			EstimateGasForActionResponse response = browser
+					.estimateGasForAction(respAction.getActionInfoList().get(0).getAction());
+
+			Logger.info(response);
+
 			Assert.assertNotNull(response);
 			Assert.assertNotNull(response.getGas());
 			Assert.assertEquals(10400L, response.getGas());
--- src/test/java/org/iotexproject/antenna/grpc/ClientTest.java
+++ src/test/java/org/iotexproject/antenna/grpc/ClientTest.java
@@ -18,11 +18,12 @@ import org.junit.Test;
 import org.pmw.tinylog.Logger;
 
 /**
- *  Copyright (c) 2019 IoTeX
- *  This is an alpha (internal) release and is not suitable for production. This source code is provided 'as is' and no
- *  warranties are given as to title or non-infringement, merchantability or fitness for purpose and, to the extent
- *  permitted by law, all liability for your use of the code is disclaimed. This source code is governed by Apache
- *  License 2.0 that can be found in the LICENSE file.
+ * Copyright (c) 2019 IoTeX This is an alpha (internal) release and is not
+ * suitable for production. This source code is provided 'as is' and no
+ * warranties are given as to title or non-infringement, merchantability or
+ * fitness for purpose and, to the extent permitted by law, all liability for
+ * your use of the code is disclaimed. This source code is governed by Apache
+ * License 2.0 that can be found in the LICENSE file.
  * 
  * @author Fabrizio Spataro <fabryprog@gmail.com>
  */
@@ -30,46 +31,46 @@ public class ClientTest {
 
 	private static final String HOST = "api.iotex.one";
 	private static final Integer PORT = 80;
-	
+
 	@Test
 	public void getAccount() {
 		final String address = "io126xcrjhtp27end76ac9nmx6px2072c3vgz6suw";
-		
+
 		GetAccountResponse response = Client.getInstance(HOST, PORT).getAccount(address);
-	    Logger.info(response);
+		Logger.info(response);
 
 		Assert.assertNotNull(response);
 		Assert.assertNotNull(response.getAccountMeta());
 		Assert.assertEquals(address, response.getAccountMeta().getAddress());
-		
+
 		Assert.assertNotNull(response.getAccountMeta().getBalance());
 		Assert.assertNotNull(response.getAccountMeta().getNonce());
 		Assert.assertNotNull(response.getAccountMeta().getPendingNonce());
 	}
-	
+
 	@Test
 	public void getChainMeta() {
 		GetChainMetaResponse response = Client.getInstance(HOST, PORT).getChainMeta();
-	    Logger.info(response);
-		
+		Logger.info(response);
+
 		Assert.assertNotNull(response);
 	}
-	
+
 	@Test
 	public void GetEpochMeta() {
 		final long epoch = 1;
 		GetEpochMetaResponse response = Client.getInstance(HOST, PORT).getEpochMeta(epoch);
-	    Logger.info(response);
-		
+		Logger.info(response);
+
 		Assert.assertNotNull(response);
 		Assert.assertNotNull(response.getEpochData());
 		Assert.assertEquals(epoch, response.getEpochData().getNum());
 	}
-	
+
 	@Test
 	public void getServerMeta() {
 		GetServerMetaResponse response = Client.getInstance(HOST, PORT).getServerMeta();
-	    Logger.info(response);
+		Logger.info(response);
 
 		Assert.assertNotNull(response);
 		Assert.assertNotNull(response.getServerMeta());
@@ -79,12 +80,12 @@ public class ClientTest {
 		Assert.assertNotNull(response.getServerMeta().getGoVersion());
 		Assert.assertNotNull(response.getServerMeta().getBuildTime());
 	}
-	
+
 	@Test
 	public void getBlockMetasByIndexLenghtOne() {
-		//INDEX
+		// INDEX
 		GetBlockMetasResponse response = Client.getInstance(HOST, PORT).getBlockMetasByIndex(10L, 1L);
-	    Logger.info(response);
+		Logger.info(response);
 
 		Assert.assertNotNull(response);
 		Assert.assertEquals(1, response.getBlkMetasList().size());
@@ -92,19 +93,19 @@ public class ClientTest {
 
 	@Test
 	public void getBlockMetasByIndexLenghtTen() {
-		//INDEX
+		// INDEX
 		GetBlockMetasResponse response = Client.getInstance(HOST, PORT).getBlockMetasByIndex(10L, 10L);
-	    Logger.info(response);
+		Logger.info(response);
 		Assert.assertNotNull(response);
 		Assert.assertEquals(10, response.getBlkMetasList().size());
 	}
 
 	@Test
 	public void getBlockMetasByIndexLenghtZero() {
-		//INDEX
+		// INDEX
 		GetBlockMetasResponse response = Client.getInstance(HOST, PORT).getBlockMetasByIndex(10L, 0L);
-	    Logger.info(response);
-	    
+		Logger.info(response);
+
 		Assert.assertNotNull(response);
 		Assert.assertEquals(0, response.getBlkMetasList().size());
 	}
@@ -112,38 +113,38 @@ public class ClientTest {
 	@Test
 	public void getBlockMetasByHash() {
 		String hash = null;
-		//Retrieve hash from blockchain
+		// Retrieve hash from blockchain
 		GetBlockMetasResponse response = Client.getInstance(HOST, PORT).getBlockMetasByIndex(10L, 1L);
 		hash = response.getBlkMetas(0).getHash();
 
 		response = Client.getInstance(HOST, PORT).getBlockMetasByHash(hash);
-	    Logger.info(response);
-		
+		Logger.info(response);
+
 		Assert.assertNotNull(response);
 		Assert.assertEquals(1, response.getBlkMetasList().size());
 		Assert.assertEquals(hash, response.getBlkMetas(0).getHash());
 	}
-	
+
 	@Test
 	public void getSuggestGasPrice() {
 		SuggestGasPriceResponse response = Client.getInstance(HOST, PORT).getSuggestGasPrice();
-	    Logger.info(response);
-	    
+		Logger.info(response);
+
 		Assert.assertNotNull(response);
 		Assert.assertEquals(1000000000000L, response.getGasPrice());
 	}
-	
+
 	@Test
 	public void readContract() {
 		Long start = 0L;
 		Long count = 30L;
 		GetActionsResponse response = Client.getInstance(HOST, PORT).getActionsByIndex(start, count);
-	    Logger.info(response);
-	    
+		Logger.info(response);
+
 		Assert.assertNotNull(response);
 		Assert.assertNotNull(response.getActionInfoList());
 		Assert.assertEquals(count.intValue(), response.getActionInfoList().size());
-		for(ActionInfo item : response.getActionInfoList()) {
+		for (ActionInfo item : response.getActionInfoList()) {
 			Assert.assertNotNull(item.getActHash());
 			Assert.assertNotNull(item.getBlkHash());
 			Assert.assertNotNull(item.getAction());
@@ -156,69 +157,77 @@ public class ClientTest {
 	@Test
 	public void getActionsByIndexOne() {
 		GetActionsResponse response = Client.getInstance(HOST, PORT).getActionsByIndex(10L, 1L);
-	    Logger.info(response);
-	    
+		Logger.info(response);
+
 		Assert.assertNotNull(response);
 		Assert.assertNotNull(response.getActionInfoList());
 		Assert.assertEquals(1, response.getActionInfoList().size());
 	}
+
 	@Test
 	public void getActionsByIndexTen() {
 		GetActionsResponse response = Client.getInstance(HOST, PORT).getActionsByIndex(10L, 10L);
-	    Logger.info(response);
-	    
+		Logger.info(response);
+
 		Assert.assertNotNull(response);
 		Assert.assertNotNull(response.getActionInfoList());
 		Assert.assertEquals(10, response.getActionInfoList().size());
 	}
+
 	@Test
 	public void getActionsByIndexZero() {
 		GetActionsResponse response = Client.getInstance(HOST, PORT).getActionsByIndex(10L, 0L);
-	    Logger.info(response);
-	    
+		Logger.info(response);
+
 		Assert.assertNotNull(response);
 		Assert.assertNotNull(response.getActionInfoList());
 		Assert.assertEquals(0, response.getActionInfoList().size());
 	}
+
 	@Test
 	public void getActionsByHash() {
-		GetActionsResponse response = Client.getInstance(HOST, PORT).getActionsByHash("eb0bab335c3ab0d43020cbe3aa494933ee473394d75a52fd5112cff6af2a5d92", false);
-	    Logger.info(response);
-	    
+		GetActionsResponse response = Client.getInstance(HOST, PORT)
+				.getActionsByHash("eb0bab335c3ab0d43020cbe3aa494933ee473394d75a52fd5112cff6af2a5d92", false);
+		Logger.info(response);
+
 		Assert.assertNotNull(response);
 		Assert.assertNotNull(response.getActionInfoList());
 		Assert.assertEquals(1, response.getActionInfoList().size());
 	}
+
 	@Test
 	public void getActionsByBlockHash() {
 		GetBlockMetasResponse response = Client.getInstance(HOST, PORT).getBlockMetasByIndex(10L, 1L);
-	    Logger.info(response);
+		Logger.info(response);
 		Assert.assertNotNull(response);
 		Assert.assertNotNull(response.getBlkMetasList());
 		Assert.assertEquals(1, response.getBlkMetasList().size());
 		String hash = response.getBlkMetasList().get(0).getHash();
 		GetActionsResponse respAction = Client.getInstance(HOST, PORT).getActionsByBlock(hash, 0L, 1L);
-	    
+
 		Assert.assertNotNull(respAction);
 		Assert.assertNotNull(respAction.getActionInfoList());
 		Assert.assertEquals(1, respAction.getActionInfoList().size());
 	}
-	//TODO TEST
+
+	// TODO TEST
 	public void estimateGasForAction() {
 		GetBlockMetasResponse respBlock = Client.getInstance(HOST, PORT).getBlockMetasByIndex(10L, 1L);
 		Assert.assertNotNull(respBlock);
 		Assert.assertNotNull(respBlock.getBlkMetasList());
 		Assert.assertEquals(1, respBlock.getBlkMetasList().size());
-		
-		GetActionsResponse respAction = Client.getInstance(HOST, PORT).getActionsByBlock(respBlock.getBlkMetasList().get(0).getHash(), 0L, 1L);
+
+		GetActionsResponse respAction = Client.getInstance(HOST, PORT)
+				.getActionsByBlock(respBlock.getBlkMetasList().get(0).getHash(), 0L, 1L);
 		Assert.assertNotNull(respAction);
 		Assert.assertNotNull(respAction.getActionInfoList());
 		Assert.assertEquals(1, respAction.getActionInfoList().size());
-		
-		EstimateGasForActionResponse response = Client.getInstance(HOST, PORT).estimateGasForAction(respAction.getActionInfoList().get(0).getAction());
-		
-	    Logger.info(response);
-	    
+
+		EstimateGasForActionResponse response = Client.getInstance(HOST, PORT)
+				.estimateGasForAction(respAction.getActionInfoList().get(0).getAction());
+
+		Logger.info(response);
+
 		Assert.assertNotNull(response);
 		Assert.assertNotNull(response.getGas());
 		Assert.assertEquals(10400L, response.getGas());
