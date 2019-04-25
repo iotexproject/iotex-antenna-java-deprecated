589c9c735d3ef1719d2638f68264621339372648
Fabryprog <fabryprog@gmail.com>
2019-04-25 23:58:40 +0200

    added SSL Support

--- src/test/java/org/iotexproject/antenna/rpcmethod/TestConstants.java
+++ src/test/java/org/iotexproject/antenna/rpcmethod/TestConstants.java
@@ -8,13 +8,14 @@ package org.iotexproject.antenna.rpcmethod;
  *
  */
 public class TestConstants {
-	public static final String HOST = "api.iotex.one";
-	public static final Integer PORT = 80;
+	public static final String HOST = "api.testnet.iotex.one";
+	public static final Integer PORT = 443;
+	public static final Boolean SSL = Boolean.TRUE;
 
 	// getAccount()
 	public static final String ADDRESS = "io126xcrjhtp27end76ac9nmx6px2072c3vgz6suw";
 
 	// getActionsByHash()
-	public static final String ACTION_HASH = "eb0bab335c3ab0d43020cbe3aa494933ee473394d75a52fd5112cff6af2a5d92";
+	public static final String ACTION_HASH = "5526eea2aac8f22afebb67058c45e55d1ddc9c4c1f8db055ec04c52edb8ed23f";
 
 }
